c1b6c15271f0dbcdc53f9af1a803e6760775aa5b
DaniilCruel <daniilcruel@users.noreply.github.com>
2019-12-05 21:48:14 +0300

    Complet Zoom

--- src/Laba_4C/GraphicsDisplay.java
+++ src/Laba_4C/GraphicsDisplay.java
@@ -2,15 +2,12 @@ package Laba_4C;
 
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.awt.font.FontRenderContext;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Paint;
-import java.awt.Stroke;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Line2D;
@@ -19,23 +16,29 @@ import java.awt.geom.Rectangle2D;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
+import java.util.ArrayList;
 
 
 import static java.lang.Math.abs;
 
 public class GraphicsDisplay extends JPanel {
 
+
+    private double[][] viewport = new double[2][2];
     private Font axisFont;             // шрифт подписей к осям коорд
+    private ArrayList<double[][]> undoHistory = new ArrayList(5);
 
-    private Double[][] graphicsData;         // список коорд точек для построения графика
-
-    // различные стили черчения линий
+    private int selectedMarker = -1;
+    private Double[][] graphicsData;
+    private Double[][] originalData;// список коорд точек для построения графика
     private BasicStroke axisStroke;
     private BasicStroke graphicsStroke;
     private BasicStroke markerStroke;
 
-
-
+    private boolean scaleMode = false;
+    private boolean changeMode = false;
+    private double[] originalPoint = new double[2];
+    private Rectangle2D.Double selectionRect = new Rectangle2D.Double();
     // границы диапазона пространства,подлежащего отображению
     private double maxX;
     private double maxY;
@@ -46,28 +49,54 @@ public class GraphicsDisplay extends JPanel {
     private boolean showAxis = true;
     private boolean showMarkers = true;
     private boolean showTurn = true;
+
     public GraphicsDisplay() {
-        // Цвет заднего фона области отображения - белый
+
         setBackground(Color.WHITE);
-        // Сконструировать необходимые объекты, используемые в рисовании
-        // Перо для рисования графика
         graphicsStroke = new BasicStroke(4.0f, BasicStroke.CAP_ROUND,
-                BasicStroke.JOIN_ROUND, 10.0f, new float[]{30,5,5,5,5,5,15,5,15,5}, 0.0f);
-        // Перо для рисования осей координат
+                BasicStroke.JOIN_ROUND, 10.0f, new float[]{30, 5, 5, 5, 5, 5, 15, 5, 15, 5}, 0.0f);
         axisStroke = new BasicStroke(3.0f, BasicStroke.CAP_ROUND,
                 BasicStroke.JOIN_MITER, 10.0f, null, 0.0f);
-        // Перо для рисования контуров маркеров
-        markerStroke = new BasicStroke(2.0f, BasicStroke.CAP_SQUARE ,
+        markerStroke = new BasicStroke(2.0f, BasicStroke.CAP_SQUARE,
                 BasicStroke.JOIN_MITER, 10.0f, null, 0.0f);
-        // Шрифт для подписей осей координат
         axisFont = new Font("Serif", Font.BOLD, 36);
+        addMouseMotionListener(new MouseMotionHandler());
+        addMouseListener(new MouseHandler());
     }
 
+
     public void showGraphics(Double[][] graphicsData) {
-        // Сохранить массив точек во внутреннем поле класса
         this.graphicsData = graphicsData;
-        // Запросить перерисовку компонента (неявно вызвать paintComponent())
-        repaint();
+
+
+        this.originalData = graphicsData;
+        this.minX = graphicsData[0][0];
+        this.maxX = graphicsData[graphicsData.length - 1][0];
+        this.minY = graphicsData[0][1];
+        this.maxY = this.minY;
+        for (int i = 1; i < graphicsData.length; i++) {
+            if (graphicsData[i][1] < this.minY) {
+                this.minY = graphicsData[i][1];
+            }
+            if (graphicsData[i][1] > this.maxY) {
+                this.maxY = graphicsData[i][1];
+            }
+        }
+        zoomToRegion(minX, maxY, maxX, minY);
+
+    }
+
+
+    protected double[] translatePointToXY(int x, int y) {
+        return new double[]{this.viewport[0][0] + x / this.scale, this.viewport[0][1] - y / this.scale};
+    }
+
+    public void zoomToRegion(double x1, double y1, double x2, double y2) {
+        this.viewport[0][0] = x1;
+        this.viewport[0][1] = y1;
+        this.viewport[1][0] = x2;
+        this.viewport[1][1] = y2;
+        this.repaint();
     }
 
     protected Point2D.Double xyToPoint(double x, double y) {
@@ -179,7 +208,7 @@ public class GraphicsDisplay extends JPanel {
             Point2D.Double labelPos = xyToPoint(maxX, 0);
             Point2D.Double labelPos2 = xyToPoint(1, 0);
             canvas.drawString("",
-                    (float) (labelPos2.getX()  ),
+                    (float) (labelPos2.getX()),
                     (float) (labelPos2.getY()));
 
             // Вывести надпись в точке с вычисленными координатами
@@ -189,28 +218,39 @@ public class GraphicsDisplay extends JPanel {
         }
     }
 
+    protected int findSelectedPoint(int x, int y) {
+        if (graphicsData == null) return -1;
+        int pos = 0;
+        for (Double[] point : graphicsData) {
+            Point2D.Double screenPoint = xyToPoint(point[0].doubleValue(), point[1].doubleValue());
+            double distance = (screenPoint.getX() - x) * (screenPoint.getX() - x) + (screenPoint.getY() - y) * (screenPoint.getY() - y);
+            if (distance < 100) return pos;
+            pos++;
+        }
+        return -1;
+    }
+
     public void Zad() {
         // Сохранить массив точек во внутреннем поле класса
         double A = 0;
         double B = 0;
         double znach = 0;
-        double znach2 =0;
+        double znach2 = 0;
         boolean T = true;
         float S = 0;
         for (Double[] point : graphicsData) {
 
 
-
-            znach2 =  znach;
+            znach2 = znach;
             znach = point[1];
 
-            if(znach*znach2<=0  && znach2 !=0) {
+            if (znach * znach2 <= 0 && znach2 != 0) {
 
 
-                T=!T;
+                T = !T;
             }
-            if(!T) {
-                S +=abs( point[1] * point[0])/5;
+            if (!T) {
+                S += abs(point[1] * point[0]) / 5;
             }
 
         }
@@ -295,11 +335,10 @@ public class GraphicsDisplay extends JPanel {
         Font oldFont = canvas.getFont();
 
 
-
         if (!showTurn) {
-            AffineTransform at = AffineTransform.getRotateInstance(-Math.PI/2, getSize().getWidth()/2, getSize().getHeight()/2);
-            at.concatenate(new AffineTransform(getSize().getHeight()/getSize().getWidth(), 0.0, 0.0, getSize().getWidth()/getSize().getHeight(),
-                    (getSize().getWidth()-getSize().getHeight())/2, (getSize().getHeight()-getSize().getWidth())/2));
+            AffineTransform at = AffineTransform.getRotateInstance(-Math.PI / 2, getSize().getWidth() / 2, getSize().getHeight() / 2);
+            at.concatenate(new AffineTransform(getSize().getHeight() / getSize().getWidth(), 0.0, 0.0, getSize().getWidth() / getSize().getHeight(),
+                    (getSize().getWidth() - getSize().getHeight()) / 2, (getSize().getHeight() - getSize().getWidth()) / 2));
             canvas.setTransform(at);
 
 
@@ -320,17 +359,17 @@ public class GraphicsDisplay extends JPanel {
         canvas.setStroke(oldStroke);
     }
 
-    public void saveToTextFile(File selectedFile)	{
-        try{
+    public void saveToTextFile(File selectedFile) {
+        try {
             PrintStream out = new PrintStream(selectedFile);
             out.println("Результаты скорректированых значений");
-            for (Double[] point : graphicsData){
+            for (Double[] point : graphicsData) {
                 out.println(point[0] + " " + point[1]);
             }
 
             out.close();
 
-        }catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
 
         }
 
@@ -347,11 +386,103 @@ public class GraphicsDisplay extends JPanel {
     }
 
 
-    public  void setTurnAction(boolean  showTurn) {
-        this.showTurn =  showTurn;
+    public void setTurnAction(boolean showTurn) {
+        this.showTurn = showTurn;
         repaint();
     }
 
+
+    public class MouseHandler extends MouseAdapter {
+        public MouseHandler() {
+        }
+
+        public void mouseClicked(MouseEvent ev) {
+            if (ev.getButton() == 3) {
+                if (undoHistory.size() > 0) {
+                    viewport = ((double[][]) undoHistory.get(undoHistory.size() - 1));
+
+                    undoHistory.remove(undoHistory.size() - 1);
+                } else {
+                    zoomToRegion(minX, maxY, maxX, minY);
+                }
+                repaint();
+            }
+        }
+
+        public void mousePressed(MouseEvent ev) {
+            if (ev.getButton() != 1) return;
+            selectedMarker = findSelectedPoint(ev.getX(), ev.getY());
+            originalPoint = translatePointToXY(ev.getX(), ev.getY());
+            if (selectedMarker >= 0) {
+                changeMode = true;
+                setCursor(Cursor.getPredefinedCursor(8));
+            } else {
+                scaleMode = true;
+                setCursor(Cursor.getPredefinedCursor(5));
+                selectionRect.setFrame(ev.getX(), ev.getY(), 1.0D, 1.0D);
+            }
+        }
+
+        public void mouseReleased(MouseEvent ev) {
+            if (ev.getButton() != 1) return;
+
+            setCursor(Cursor.getPredefinedCursor(0));
+            if (changeMode) {
+                changeMode = false;
+            } else {
+                scaleMode = false;
+                double[] finalPoint = translatePointToXY(ev.getX(), ev.getY());
+                undoHistory.add(viewport);
+                viewport = new double[2][2];
+                zoomToRegion(originalPoint[0], originalPoint[1], finalPoint[0], finalPoint[1]);
+                repaint();
+            }
+        }
+    }
+
+
+    public class MouseMotionHandler implements MouseMotionListener {
+
+        public void mouseDragged(MouseEvent ev) {
+            if (changeMode) {
+
+                double[] currentPoint = translatePointToXY(ev.getX(), ev.getY());
+                double newY = ((Double[]) graphicsData[selectedMarker])[1].doubleValue() +
+                        (currentPoint[1] - ((Double[]) graphicsData[selectedMarker])[1].doubleValue());
+                if (newY > viewport[0][1]) {
+                    newY = viewport[0][1];
+                }
+                if (newY < viewport[1][1]) {
+                    newY = viewport[1][1];
+                }
+                ((Double[]) graphicsData[selectedMarker])[1] = Double.valueOf(newY);
+                repaint();
+            } else {
+                double width = ev.getX() - selectionRect.getX();
+                if (width < 5.0D) {
+                    width = 5.0D;
+                }
+                double height = ev.getY() - selectionRect.getY();
+                if (height < 5.0D) {
+                    height = 5.0D;
+                }
+                selectionRect.setFrame(selectionRect.getX(), selectionRect.getY(), width, height);
+                repaint();
+            }
+        }
+
+
+        public void mouseMoved(MouseEvent ev) {
+            selectedMarker = findSelectedPoint(ev.getX(), ev.getY());
+            if (selectedMarker >= 0)
+                setCursor(Cursor.getPredefinedCursor(8));
+            else {
+                setCursor(Cursor.getPredefinedCursor(0));
+            }
+            repaint();
+        }
+
+    }
 }
 
 
